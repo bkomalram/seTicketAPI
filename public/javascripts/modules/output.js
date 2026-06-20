215f9b4e3fd491c13fac0f17252a443a29d90749
Bhagwan Komalram <bhagwan.ram.komalram@gmail.com>
2026-06-20 13:34:04 -0500

    Cargarsorteo v2

--- public/javascripts/modules/output.js
+++ public/javascripts/modules/output.js
@@ -155,7 +155,7 @@ const loadOldTicket = async function (num) {
 }
 
 const showConfiguracion = function () {
-    cargarEstadoSorteo();
+    //cargarEstadoSorteo();
     document.querySelector("#venta").classList.add('d-none')    
     document.querySelector("#configuracion").classList.remove('d-none')    
 }
